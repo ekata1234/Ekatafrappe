d47d80ee317552018e7e38897db6caa4c3bb46b7
abhishekprakash32 <abhishekp.ext@elastic.run>
2023-02-14 19:10:20 +0530

    Fixed Print Format in SI & Also Added in DN

---
+++ ekata/ekata/custom_scripts/delivery_note/delivery_note.js
@@ -0,0 +1,18 @@
+frappe.ui.form.on("Delivery Note", "refresh", function(frm) {
+    
+	var wrapper = frm.get_field("preview_html").$wrapper;
+	var is_viewable = frappe.utils.is_image_file(frm.doc.attachment);
+
+	frm.toggle_display("preview", is_viewable);
+	frm.toggle_display("preview_html", is_viewable);
+
+	if(is_viewable){
+		wrapper.html('<div class="img_preview">\
+			<img class="img-responsive" src="'+frm.doc.attachment+'"></img>\
+			</div>');
+            frm.set_df_property('authorized_by', 'hidden', 1)
+	} else {
+		wrapper.empty();
+        frm.set_df_property('authorized_by', 'hidden', 0)
+	}
+});
