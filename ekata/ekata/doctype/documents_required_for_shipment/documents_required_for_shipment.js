be406493b608b4d6ed511982201855d0a8901492
DPriyanka123 <priyanka.d@indictrans.in>
2023-05-25 17:08:13 +0530

    feat:Added custom doctype for shipping documents

---
+++ ekata/ekata/doctype/documents_required_for_shipment/documents_required_for_shipment.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, kiran.c@indictrans.in and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Documents Required for Shipment', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ ekata/ekata/doctype/documents_required_for_shipment/test_documents_required_for_shipment.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, kiran.c@indictrans.in and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestDocumentsRequiredforShipment(unittest.TestCase):
+	pass
