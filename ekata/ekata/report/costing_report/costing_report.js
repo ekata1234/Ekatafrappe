b10599ed3ff98ba8e0362493af0bec309ef7f4e2
DPriyanka123 <priyanka.d@indictrans.in>
2023-07-03 11:23:05 +0530

    feat:Added custom report for A/C costing

---
+++ ekata/ekata/report/costing_report/costing_report.js
@@ -0,0 +1,34 @@
+// Copyright (c) 2023, kiran.c@indictrans.in and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Costing Report"] = {
+	"filters": [
+		{
+			"fieldname":"from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"width": "80",
+			"reqd": 0,
+			
+		},
+		{
+			"fieldname":"to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"width": "80",
+			"reqd": 0,
+			
+		},
+		{
+			"fieldname":"project",
+			"label": __("Project"),
+			"fieldtype": "Link",
+			"options":"Project",
+			"width": "80",
+			"reqd": 0,
+			
+		},
+
+	]
+};
